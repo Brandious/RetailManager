f593a68dac18ea5653b2153f5c3e3a55cbc27506
adnan facic <adnanfacic96@gmail.com>
2020-04-15 22:41:14 +0200

    Register DB tables
    Created SQL Tables for Register System

---
+++ RMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+    [PurchasePrice] MONEY NOT NULL , 
+   
+)
