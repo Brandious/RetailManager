c00cefe447bd92ac63a8fb165a82e80327ec51d5
adnan facic <adnanfacic96@gmail.com>
2020-04-16 23:44:17 +0200

    Designed Sales Screen

---
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,132 @@
+﻿using Caliburn.Micro;
+using System.ComponentModel;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+		private BindingList<string> _products;
+
+		public BindingList<string> Products
+		{
+			get { return _products; }
+			set 
+			{ 
+				_products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		private string _itemQuantity;
+
+		public string ItemQuantity
+		{
+			get { return _itemQuantity; }
+			set 
+			{
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		private BindingList<string> _cart;
+
+		public BindingList<string> Cart
+		{
+			get { return _cart; }
+			set 
+			{ 
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+
+		public string SubTotal
+		{
+			get 
+			{
+				//Add Calculation
+				return "$ 0.00";
+			}
+
+		}
+
+
+		public string Tax
+		{
+			get
+			{
+				//Add Calculation
+				return "$ 0.00";
+			}
+
+		}
+
+		public string Total
+		{
+			get
+			{
+				//Add Calculation
+				return "$ 0.00";
+			}
+
+		}
+
+		public bool CanAddToCart
+		{
+			get
+			{
+				bool output = false;
+
+				//Make sure something is Selected
+				//Make sure ItemQuantity true
+
+				return output;
+			}
+		}
+
+		public bool CanRemoveFromCart
+		{
+			get
+			{
+				bool output = false;
+
+				//Make sure something is Selected
+				//Make sure ItemQuantity true
+
+				return output;
+			}
+		}
+
+		public bool CanCheckOut
+		{
+			get
+			{
+				bool output = false;
+
+				//Make sure something is Selected
+				//Make sure ItemQuantity true
+
+				return output;
+			}
+		}
+
+		public void AddToCart()
+		{
+
+		}
+
+		public void RemoveFromCart()
+		{
+
+		}
+
+		public void CheckOut()
+		{
+
+		}
+
+
+	}
+}
