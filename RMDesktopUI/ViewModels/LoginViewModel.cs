7618bfe66c309c10d796cecfd4979822df891e1d
adnan facic <adnanfacic96@gmail.com>
2020-04-16 11:59:58 +0200

    Added Error Handling to Login form

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -38,6 +38,35 @@ namespace RMDesktopUI.ViewModels
 			}
 		}
 
+		
+		public bool IsErrorVisible
+		{
+		
+			get
+			{
+				bool output = false;
+				
+				if (ErrorMessage?.Length > 0)
+					output = true;
+
+				return output;
+			}
+		}
+
+		private string  _errorMessage;	
+
+		public string  ErrorMessage
+		{
+			get { return _errorMessage; }
+			set
+			{ 
+				_errorMessage = value;
+				NotifyOfPropertyChange(() => ErrorMessage);
+				NotifyOfPropertyChange(() => IsErrorVisible);
+			}
+		}
+
+
 		public bool CanLogIn
 		{
 			get
@@ -55,11 +84,12 @@ namespace RMDesktopUI.ViewModels
 		{
 			try
 			{
+				ErrorMessage = "";
 				var result = await _apiHelper.Authenticate(UserName, Password);
 			}
 			catch(Exception ex)
 			{
-				Console.WriteLine(ex.Message);
+				ErrorMessage = ex.Message;
 			}
 		}
 
